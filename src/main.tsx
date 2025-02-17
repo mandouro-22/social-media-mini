958cf284b0055587ba2b57e7cd8be68a6f85a817
omar-mandour <mandouro383@gmail.com>
2025-02-17 14:34:39 +0200

    test_client_id

--- src/main.tsx
+++ src/main.tsx
@@ -8,11 +8,11 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 // react query
 const queeryClient = new QueryClient();
-// 844929440938-u3fg7bdc1q8us5dtldefjmtuc29odfm4.apps.googleusercontent.com
+// 1024532969830-6r9826loqvct34be4eu52qbca8pt0us2.apps.googleusercontent.com
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queeryClient}>
-      <GoogleOAuthProvider clientId="1024532969830-6r9826loqvct34be4eu52qbca8pt0us2.apps.googleusercontent.com">
+      <GoogleOAuthProvider clientId="94142909577-8qav3d7hpcms630ej23g1d86rurcvqun.apps.googleusercontent.com">
         <App />
       </GoogleOAuthProvider>
     </QueryClientProvider>
