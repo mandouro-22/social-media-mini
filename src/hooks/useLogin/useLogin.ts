43dc0bd0496ebaf45a3409bf890917c7cb79a0ec
omar-mandour <mandouro383@gmail.com>
2025-02-20 19:41:16 +0200

    fix-bug-login

--- src/hooks/useLogin/useLogin.ts
+++ src/hooks/useLogin/useLogin.ts
@@ -1,9 +1,12 @@
 import { useMutation } from "@tanstack/react-query";
 import { LoginValues } from "../../types/auth";
-import { apiClient } from "../../config/axiosConfig";
+import axios from "axios";
 
 const sendData = async (data: LoginValues): Promise<LoginValues> => {
-  const response = await apiClient.post("/auth/signin", data);
+  const response = await axios.post(
+    "https://delightful-shelli-social-media-app-437a3548.koyeb.app/api/auth/signin",
+    data
+  );
   return response.data;
 };
 
