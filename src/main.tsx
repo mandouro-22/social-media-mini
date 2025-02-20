babbeb7a9e7e8269274273069a38ba901dfae344
omar-mandour <mandouro383@gmail.com>
2025-02-20 15:56:30 +0200

    fix-bug

--- src/main.tsx
+++ src/main.tsx
@@ -8,11 +8,14 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queeryClient = new QueryClient();
-const Client_ID = import.meta.env.VITE_APP_CLINET_ID_USED;
+// const Client_ID = import.meta.env.VITE_APP_CLINET_ID_USED;
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queeryClient}>
-      <GoogleOAuthProvider clientId={Client_ID}>
+      <GoogleOAuthProvider
+        clientId={
+          "1024532969830-6r9826loqvct34be4eu52qbca8pt0us2.apps.googleusercontent.com"
+        }>
         <App />
       </GoogleOAuthProvider>
       <ReactQueryDevtools initialIsOpen={false} />
